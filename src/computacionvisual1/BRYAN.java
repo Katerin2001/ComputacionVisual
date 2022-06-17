1eac7354dffa5b23c3c1c874932a2ace52c12e55
Bryan <boviedo4550@uta.edu.ec>
2022-06-17 13:10:21 -0500

    Bryan

---
+++ src/computacionvisual1/BRYAN.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package computacionvisual1;
+
+/**
+ *
+ * @author BRYAN
+ */
+public class BRYAN {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
