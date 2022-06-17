0b6175ab89275a6ada98763987a7dd8c891d0d6a
Bryan <boviedo4550@uta.edu.ec>
2022-06-17 14:19:06 -0500

    Añadido

--- src/computacionvisual1/KaterinCecen.java
+++ src/computacionvisual1/KaterinCecen.java
@@ -17,6 +17,7 @@ public class KaterinCecen {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("");
+        System.out.println("BO");
     }
     
 }
